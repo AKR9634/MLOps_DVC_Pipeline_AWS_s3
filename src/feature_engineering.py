8fb354202a14f7e894c86b6397971fbb0114f873
AKR9634 <akhil6july2003@gmail.com>
2026-01-11 14:55:10 +0530

    pipeline configured using DVC, with Params

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -4,6 +4,7 @@ import os
 from sklearn.model_selection import train_test_split
 import logging
 from sklearn.feature_extraction.text import TfidfVectorizer
+import yaml
 
 # Ensure the "logs" directory exists 
 log_dir = 'logs'
@@ -27,6 +28,30 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+# YAML PARAMS LOADING
+def load_params(params_path : str) ->dict:
+    """Load parameters from a YAML file"""
+
+    try:
+
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+
+    except FileNotFoundError:
+        logger.error('File not Found: %s', params_path)
+        raise
+    
+    except yaml.YAMLError as e:
+        logger.error('YAML Error : %s', e)
+        raise
+
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(file_path : str) -> pd.DataFrame:
@@ -92,8 +117,8 @@ def save_data(df : pd.DataFrame, file_path : str) -> None:
 def main():
 
     try:
-
-        max_features = 50
+        params = load_params(params_path='params.yaml')
+        max_features = params['feature_engineering']['max_features']
 
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
